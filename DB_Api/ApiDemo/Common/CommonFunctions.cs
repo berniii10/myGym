a46434ee5c073cef57865d352320631529daf1cf
Bernat Pujol <bernat.pujol@keysight.com>
2024-06-22 13:38:43 +0200

    Created function to search for a unique ID, and use that ID to add an exercise, so IDs can be re-used

---
+++ DB_Api/ApiDemo/Common/CommonFunctions.cs
@@ -0,0 +1,20 @@
+﻿namespace ApiDemo.Common
+{
+    static public class CommonFunctions
+    {
+        public static int getUniqueId(List<int> ids)
+        {
+            int expected = 1; // Initialize the expected value
+
+            for (int i = 0; i < ids.Count; i++)
+            {
+                if (ids[i] == expected)
+                    expected++; // Increment expected if the current ID matches
+                else
+                    break; // Break the loop if a missing ID is found
+            }
+
+            return expected;
+        }
+    }
+}
